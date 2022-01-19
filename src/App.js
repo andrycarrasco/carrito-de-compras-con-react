72f15f136a11a9c4b01ded25446f796304246685
andrycarrasco <andryjuca@gmail.com>
2022-01-19 17:43:35 -0300

    proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Productos from './components/Productos';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
+import Title from './components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    productos: [
+      {name: 'Tomate', price: 1250, img: '/productos/tomate.jpg'},
+      {name: 'Arbejas', price: 1200, img: '/productos/arbejas.jpg'},
+      {name: 'Lechuga', price: 1500, img: '/productos/lechuga.jpg'}
+    ],
+    carro: [],
+    esCarroVisible: false
+  }
+  mostrarCarro = () => {
+    if(!this.state.carro.length){
+      return
+    }
+    this.setState({esCarroVisible: !this.state.esCarroVisible});
+  }
+  agregarAlCarro = (producto) => {
+    const {carro} = this.state
+    if(carro.find(x => x.name === producto.name)){
+      const newCarro = carro.map(x => x.name === producto.name ? ({...x, cantidad: x.cantidad++}) : x)
+      return this.setState(newCarro)
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+       cantidad: 1,
+      })
+    })
+  }
+  render() {
+    return (
+       <div>
+         <Navbar 
+          carro={this.state.carro} 
+          esCarroVisible={this.state.esCarroVisible} 
+          mostrarCarro={this.mostrarCarro}/>
+         <Layout>  
+           <Title/>
+          <Productos 
+            productos = {this.state.productos}
+            agregarAlCarro = {this.agregarAlCarro}
+            />
+         </Layout>
+       </div>
+    );
+  }
 }
 
 export default App;
